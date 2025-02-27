c60c8eb1e9d3c0fb9d51e9b4864bb4fcaa9f1500
Heshan Padamsiri <hpheshan@gmail.com>
2025-02-27 10:18:40 +0530

    Generate ballerina types from model

--- src/main/java/ballerina/BallerinaModel.java
+++ src/main/java/ballerina/BallerinaModel.java
@@ -5,43 +5,58 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-
 public record BallerinaModel(DefaultPackage defaultPackage, List<Module> modules) {
 
     public record DefaultPackage(String org, String name, String version) {
+
     }
 
     public record Module(String name, List<TextDocument> textDocuments) {
+
     }
 
     public record TextDocument(String documentName, List<Import> imports, List<ModuleTypeDef> moduleTypeDefs,
                                List<ModuleVar> moduleVars, List<Listener> listeners, List<Service> services,
                                List<Function> functions, List<String> Comments) {
+
     }
 
     public record Import(String orgName, String moduleName, Optional<String> importPrefix) {
+
     }
 
     public record ModuleTypeDef(String name, TypeDesc typeDesc) {
+
     }
 
     public sealed interface TypeDesc {
 
-        record RecordTypeDesc(List<RecordField> fields, TypeDesc rest) implements TypeDesc {
+        record RecordTypeDesc(List<TypeDesc> inclusions, List<RecordField> fields, Optional<TypeDesc> rest)
+                implements TypeDesc {
 
             private static final String INDENT = "  ";
 
             @Override
             public String toString() {
                 StringBuilder sb = new StringBuilder();
-                sb.append("record {").append("\n");
+                boolean inclusiveRecord = rest.isEmpty();
+                if (inclusiveRecord) {
+                    sb.append("record {").append("\n");
+                } else {
+                    sb.append("record {|").append("\n");
+                }
+                for (TypeDesc inclusion : inclusions) {
+                    sb.append(INDENT).append("include ").append(inclusion).append("\n");
+                }
                 for (RecordField field : fields) {
                     sb.append(INDENT).append(field).append("\n");
                 }
-                if (rest != BuiltinType.ANYDATA) {
-                    sb.append(INDENT).append("...").append(rest).append("\n");
+                rest.ifPresent(typeDesc -> sb.append(INDENT).append("...").append(typeDesc).append("\n"));
+                if (inclusiveRecord) {
+                    sb.append("}");
+                } else {
+                    sb.append("|}");
                 }
-                sb.append("}");
                 return sb.toString();
             }
 
@@ -62,15 +77,20 @@ public record BallerinaModel(DefaultPackage defaultPackage, List<Module> modules
             }
         }
 
-        record UnionTypeDesc(Collection<TypeDesc> members) implements TypeDesc {
+        record UnionTypeDesc(Collection<? extends TypeDesc> members) implements TypeDesc {
 
             public static UnionTypeDesc of(TypeDesc... members) {
                 return new UnionTypeDesc(List.of(members));
             }
+
+            @Override
+            public String toString() {
+                return String.join(" | ", members.stream().map(Object::toString).toList());
+            }
         }
 
         enum BuiltinType implements TypeDesc {
-            ANYDATA("anydata");
+            ANYDATA("anydata"), NIL("()"), STRING("string");
 
             private final String name;
 
@@ -87,14 +107,17 @@ public record BallerinaModel(DefaultPackage defaultPackage, List<Module> modules
     }
 
     public record ModuleVar(String name, String type, BallerinaExpression expr) {
+
     }
 
     public record Service(String basePath, List<String> listenerRefs, List<Resource> resources,
                           List<Function> functions, List<String> pathParams, List<String> queryParams) {
+
     }
 
     // TODO: move port to config map
     public record Listener(ListenerType type, String name, String port, Map<String, String> config) {
+
     }
 
     public enum ListenerType {
@@ -103,35 +126,44 @@ public record BallerinaModel(DefaultPackage defaultPackage, List<Module> modules
 
     public record Resource(String resourceMethodName, String path, List<Parameter> parameters,
                            Optional<String> returnType, List<Statement> body) {
+
     }
 
     public record Function(Optional<String> visibilityQualifier, String methodName, List<Parameter> parameters,
-                           Optional<String> returnType,
-                           List<Statement> body) {
+                           Optional<String> returnType, List<Statement> body) {
+
     }
 
     public record Parameter(String name, String type, Optional<BallerinaExpression> defaultExpr) {
+
     }
 
     public record BallerinaStatement(String stmt) implements Statement {
+
     }
 
     public record BallerinaExpression(String expr) {
+
     }
 
     public record IfElseStatement(BallerinaExpression ifCondition, List<Statement> ifBody,
                                   List<ElseIfClause> elseIfClauses, List<Statement> elseBody) implements Statement {
+
     }
 
     public record ElseIfClause(BallerinaExpression condition, List<Statement> elseIfBody) {
+
     }
 
     public record DoStatement(List<Statement> doBody, Optional<OnFailClause> onFailClause) implements Statement {
+
     }
 
     public record OnFailClause(List<Statement> onFailBody) {
+
     }
 
     public sealed interface Statement permits BallerinaStatement, IfElseStatement, DoStatement {
+
     }
 }
